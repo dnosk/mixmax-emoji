2da45074815270ee2aaa19bc6b3552cf5da6b27c
Daniel Noskin <dnosk@aol.com>
2017-03-08 16:18:21 -0800

    Hello World

---
+++ api/resolver.js
@@ -0,0 +1,38 @@
+var request = require('request');
+var _ = require('underscore');
+var emoji = require('emojilib')
+
+// The API that returns the in-email representation.
+module.exports = function(req, res) {
+  var term = req.query.text.trim();
+  if (!term) {
+    res.json([{
+      title: '<i>(enter a search term)</i>',
+      text: ''
+    }]);
+    return;
+  }
+
+  // See if any emoji's match the term query
+  var results = [];
+  for (var i = 0; i < emoji.ordered.length; i++) {
+    if (emoji.ordered[i].indexOf(term) == 0) {
+      results.push(emoji.ordered[i]);
+    }
+  }
+
+  // Send back results
+  if (results.length === 0) {
+    res.json([{
+      title: '<i>(no results)</i>',
+      text: ''
+    }]);
+  } else {
+    results.map(function(result) {
+      res.json({
+        title: emoji.lib[result].char,
+        text: result
+      });
+    })
+  }
+}
---
+++ api/typeahead.js
@@ -0,0 +1,41 @@
+var request = require('request');
+var _ = require('underscore');
+var emoji = require('emojilib')
+
+// The Type Ahead API
+module.exports = function(req, res) {
+  var term = req.query.text.trim();
+  if (!term) {
+    res.json([{
+      title: '<i>(enter a search term)</i>',
+      text: ''
+    }]);
+    return;
+  }
+
+  // See if any emoji's match the term query
+  var results = [];
+  for (var i = 0; i < emoji.ordered.length; i++) {
+    if (emoji.ordered[i].indexOf(term) == 0) {
+      results.push(emoji.ordered[i]);
+    }
+  }
+
+  // Send back results
+  if (results.length === 0) {
+    res.json([{
+      title: '<i>(no results)</i>',
+      text: ''
+    }]);
+  } else {
+    var response = _.chain(results)
+      .map(function(result) {
+        return {
+          title: emoji.lib[result].char,
+          text: result
+        };
+      })
+      .value();
+    res.json(response);
+  }
+};
